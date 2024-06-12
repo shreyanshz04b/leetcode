44dfb89ceb157122671b1fce838d2807ebf72495
SHREYANSH AGARWAL <124380884+shreyanshz04b@users.noreply.github.com>
2024-06-12 19:11:56 +0530

    Create 12JUNE:-SORT-COLORS.cpp

---
+++ 100DAYSOFCODING/12JUNE:-SORT-COLORS.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        static const int target = 1;
+        for (int i = 0, left = 0, right = nums.size() - 1; i <= right;) {
+            if (nums[i] > target) {
+                swap(nums[i], nums[right--]);
+            } else {
+                if (nums[i] < target) {
+                    swap(nums[left++], nums[i]);
+                } 
+                ++i;
+            }
+        }
+    }
+};
