face09a8227398d42452beb3fca168c844696622
SHREYANSH AGARWAL <124380884+shreyanshz04b@users.noreply.github.com>
2024-06-13 19:26:31 +0530

    Create 13JUNE:-minimum-number-of-moves-to-seat-everyone.cpp

---
+++ 100DAYSOFCODING/13JUNE:-minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(begin(seats), end(seats));
+        sort(begin(students), end(students));
+        int result = 0;
+        for (int i = 0; i < size(seats); ++i) {
+            result += abs(seats[i] - students[i]);
+        }
+        return result;
+    }
+};
