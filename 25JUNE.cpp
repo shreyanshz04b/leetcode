993e658fb1bff246ef2cda77178cd7cde299af4c
SHREYANSH AGARWAL <124380884+shreyanshz04b@users.noreply.github.com>
2024-06-25 17:27:58 +0530

    Create 25JUNE.cpp

---
+++ 25JUNE.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int prev = 0;
+        return bstToGstHelper(root, &prev);
+    }
+
+private:
+    TreeNode* bstToGstHelper(TreeNode* root, int *prev) {
+        if (!root) {
+            return root;
+        }
+        bstToGstHelper(root->right, prev);
+        root->val += *prev;
+        *prev = root->val;
+        bstToGstHelper(root->left, prev);
+        return root;
+    }
+};
